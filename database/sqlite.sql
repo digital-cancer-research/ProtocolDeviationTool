dae02e4be4424335a458f54897fddde640a3bbe6
HamzahAbbasi1997 <hamzah.abbasi@manchester.ac.uk>
2023-10-10 14:29:46 +0100

    sqlite database support

---
+++ database/sqlite.sql
@@ -0,0 +1,178 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE role (
+  role_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  role_name TEXT NOT NULL
+);
+
+CREATE TABLE team (
+  team_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  team_name TEXT NOT NULL
+);
+
+CREATE TABLE user_account (
+  user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  role_id INTEGER,
+  is_site BOOLEAN,
+  is_sponsor BOOLEAN,
+  FOREIGN KEY (role_id) REFERENCES role (role_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE user_team (
+  user_team_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  team_id INTEGER,
+  FOREIGN KEY (user_id) REFERENCES user_account (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (team_id) REFERENCES team (team_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE study (
+  study_id TEXT PRIMARY KEY,
+  study_name TEXT NOT NULL
+);
+
+CREATE TABLE team_study_access (
+  team_study_access_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  team_id INTEGER,
+  study_id TEXT,
+  FOREIGN KEY (team_id) REFERENCES team (team_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (study_id) REFERENCES study (study_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE user_study_access (
+  user_study_access_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  study_id TEXT,
+  FOREIGN KEY (user_id) REFERENCES user_account (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (study_id) REFERENCES study (study_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE data_entry (
+  entry_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  study_id TEXT NOT NULL,
+  site_id TEXT NOT NULL,
+  dvspondes_id INTEGER,
+  usubjid TEXT,
+  dventc TEXT,
+  dventcviz TEXT,
+  dvsponsev TEXT,
+  impor INTEGER,
+  at TEXT,
+  at_date TIMESTAMP,
+  domain TEXT,
+  dvseq INTEGER,
+  dvrefid INTEGER,
+  dvendtc TIMESTAMP,
+  dvcatid INTEGER,
+  dvdecodid INTEGER,
+  adv TEXT,
+  nonadv TEXT,
+  dvs_cat TEXT,
+  dvstdtc TIMESTAMP,
+  user_id INTEGER,
+  category_id INTEGER,
+  FOREIGN KEY (dvspondes_id) REFERENCES dvspondes (dvspondes_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (category_id) REFERENCES pd_category (category_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE dvspondes (
+  dvspondes_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  dvspondes_value TEXT NOT NULL
+);
+
+CREATE TABLE pd_category (
+  category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  dvcat TEXT NOT NULL,
+  dvdecod TEXT NOT NULL,
+  dvterm TEXT NOT NULL
+);
+
+-- Insert roles
+INSERT INTO role (role_name) VALUES
+  ('Admin'),
+  ('User');
+
+-- Insert users with roles
+INSERT INTO user_account (username, role_id, is_site, is_sponsor) VALUES
+  ('AdminWithAllRoles', 1, 1, 1),
+  ('AdminWithSiteRole', 1, 1, 0),
+  ('AdminWithSponsorRole', 1, 0, 1),
+  ('AdminWithNoSpecialRoles', 1, 0, 0),
+  ('UserWithAllRoles', 2, 1, 1),
+  ('UserWithSiteRole', 2, 1, 0),
+  ('UserWithSponsorRole', 2, 0, 1),
+  ('UserWithNoSpecialRoles', 2, 0, 0);
+
+-- Insert category data
+INSERT INTO pd_category (dvcat, dvdecod, dvterm)
+VALUES
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'OUT OF WINDOW - PK COLLECTION', 'Protocol deviation where the subject''s PK sample was collected out of the protocol-specified time window'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'OUT OF WINDOW - TREATMENT ADMINISTRATION', 'Protocol deviation where the subject was administered treatment outside of the protocol-specified time window'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'OUT OF WINDOW - BIOMARKER COLLECTION OR EXPLORATORY ASSESSMENT', 'Protocol deviation where the subject''s biomarker sample collection or exploratory assessment was conducted outside of the protocol-specified time window'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'OUT OF WINDOW - EFFICACY ASSESSMENT', 'Protocol deviation where the subject''s efficacy assessment was conducted outside of the protocol-specified time window'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'OUT OF WINDOW - ECG', 'Protocol deviation where the subject''s ECG safety assessment was conducted outside of the protocol-specified time window'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'OUT OF WINDOW - VITAL SIGNS', 'Protocol deviation where the subject''s vital signs assessment was conducted outside of the protocol-specified time window'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'OUT OF WINDOW - BLOODS LOCAL', 'Protocol deviation where the subject''s local blood assessment was conducted outside of the protocol-specified time window'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'OUT OF WINDOW - BLOODS CENTRAL', 'Protocol deviation where the subject''s central blood assessment was conducted outside of the protocol-specified time window'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'OUT OF WINDOW - OTHER', 'Protocol deviation where the subject''s assessment was conducted outside of the protocol-specified time window'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'MISSED ASSESMENT - PK COLLECTION', 'Protocol deviation where the subject''s PK collection assessment was not performed'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'MISSED ASSESMENT - TREATMENT ADMINISTRATION', 'Protocol deviation where the subject''s PK treatment was not performed'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'MISSED ASSESMENT - BIOMARKER COLLECTION OR EXPLORATORY ASSESSMENT', 'Protocol deviation where the subject''s BIOMARKER COLLECTION OR EXPLORATORY assessment was not performed'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'MISSED ASSESMENT - EFFICACY ASSESSMENT', 'Protocol deviation where the subject''s EFFICACY ASSESSMENT was not performed'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'MISSED ASSESMENT - ECG', 'Protocol deviation where the subject''s ECG safety assessment was not performed'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'MISSED ASSESMENT- VITAL SIGNS', 'Protocol deviation where the subject''s Vital Signs assessment was not performed'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'MISSED ASSESMENT - BLOODS LOCAL', 'Protocol deviation where the subject''s local bloods assessment was not performed'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'MISSED ASSESMENT - BLOODS CENTRAL', 'Protocol deviation where the subject''s central bloods assessment was not performed'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'MISSED ASSESSMENT - OTHER', 'Protocol deviation where the subject''s protocol-specified study assessment was not performed'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'ASSESSMENT NOT PROPERLY PERFORMED', 'Protocol deviation where the subject''s protocol-specified study assessment was not properly performed'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'ASSESSMENT PERFORMED OUT OF ORDER', 'Protocol deviation where one or more of the subject''s protocol-specified study assessments were performed out of order'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'INCOMPLETE ASSESSMENT', 'Protocol deviation where the subject''s protocol-specified study assessment was not completely performed'),
+  ('ASSESSMENT OR TIME POINT COMPLETION', 'OTHER ASSESSMENT OR TIME POINT WINDOW', 'Any other protocol deviation associated with a protocol-specified assessment or time point window'),
+  ('ELIGIBILITY CRITERIA NOT MET', 'ELIGIBILITY CRITERIA NOT MET', 'Eligibility criteria not met'),
+  ('EXCLUDED MEDICATION, VACCINE OR DEVICE', 'MEDICATION, EXCLUDED BY THE PROTOCOL, WAS ADMINISTERED', 'Protocol Deviation where the subject used a medication excluded by the protocol.'),
+  ('EXCLUDED MEDICATION, VACCINE OR DEVICE', 'VACCINE, EXCLUDED BY THE PROTOCOL, WAS ADMINISTERED', 'Protocol Deviation where the subject was administered a vaccine excluded by the protocol.'),
+  ('EXCLUDED MEDICATION, VACCINE OR DEVICE', 'DEVICE, EXCLUDED BY THE PROTOCOL, WAS ADMINISTERED', 'Protocol Deviation where the subject used a medical device excluded by the protocol.'),
+  ('EXCLUDED MEDICATION, VACCINE OR DEVICE', 'OTHER EXCLUDED MEDICATION, VACCINE OR DEVICE DEVIATION', 'Protocol Deviation where the subject used any other medication, vaccine or device excluded by the protocol.'),
+  ('FAILURE TO REPORT SAFETY EVENTS PER PROTOCOL', 'SAE NOT REPORTED WITHIN THE EXPECTED TIME FRAME', 'A protocol deviation where a serious adverse event is not reported within the protocol-specified timeframe.'),
+  ('FAILURE TO REPORT SAFETY EVENTS PER PROTOCOL', 'AES OF SPECIAL INTEREST', 'Protocol deviation where a protocol defined adverse event of special interest was not reported per protocol.'),
+  ('FAILURE TO REPORT SAFETY EVENTS PER PROTOCOL', 'FAILURE TO CONFIRM CAUSALITY ASSESSMENT WITHIN THE EXPECTED TIME FRAME', 'A protocol deviation where causality assessment was not completed within the protocol-specified timeframe'),
+  ('FAILURE TO REPORT SAFETY EVENTS PER PROTOCOL', 'LIVER FUNCTION ABNORMALITIES PER PROTOCOL', 'A protocol deviation where a liver function abnormality was not reported per protocol.'),
+  ('FAILURE TO REPORT SAFETY EVENTS PER PROTOCOL', 'PREGNANCY', 'A protocol deviation where a pregnancy was not reported per protocol.'),
+  ('FAILURE TO REPORT SAFETY EVENTS PER PROTOCOL', 'OTHER', 'Any other protocol deviation associated with the failure to report safety events per protocol.'),
+  ('INFORMED CONSENT', 'INFORMED CONSENT/ASSENT NOT SIGNED AND/OR DATED BY SUBJECT (PARENT/LEGAL REPRESENTATIVE, IF APPLICABLE)', 'Protocol deviation where the subject''s informed consent/assent was not signed and/or dated by the subject (parent/legal representative, if applicable)'),
+  ('INFORMED CONSENT', 'INFORMED CONSENT/ASSENT NOT SIGNED AND/OR DATED BY APPROPRIATE SITE STAFF', 'Protocol deviation where the subject''s informed consent/assent was not signed and/or dated by appropriate site staff'),
+  ('INFORMED CONSENT', 'INFORMED CONSENT/ASSENT NOT SIGNED PRIOR TO ANY STUDY PROCEDURE', 'Protocol deviation where the subject''s informed consent/assent was not signed prior to any study procedure'),
+  ('INFORMED CONSENT', 'SIGNED INFORMED CONSENT/ASSENT NOT AVAILABLE ON SITE', 'Protocol deviation where the subject''s signed informed consent/assent was not available at the study site'),
+  ('INFORMED CONSENT', 'WRONG INFORMED CONSENT/ASSENT VERSION SIGNED', 'Protocol deviation where the subject signed the wrong informed consent/assent version'),
+  ('INFORMED CONSENT', 'OTHER INFORMED CONSENT/ASSENT DEVIATION', 'Any other protocol deviation related to the subject''s informed consent/assent'),
+  ('NOT WITHDRAWN AFTER DEVELOPING WITHDRAWAL CRITERIA', 'NOT DISCONTINUED FROM STUDY TREATMENT', 'Protocol Deviation where a subject was not discontinued from study treatment after meeting requirement of the withdrawal criteria'),
+  ('NOT WITHDRAWN AFTER DEVELOPING WITHDRAWAL CRITERIA', 'NOT WITHDRAWN FROM STUDY', 'Protocol Deviation where a subject was not withdrawn from the study after meeting requirement of the withdrawal criteria'),
+  ('NOT WITHDRAWN AFTER DEVELOPING WITHDRAWAL CRITERIA', 'OTHER DEVIATION OF NOT BEING WITHDRAWN AFTER DEVELOPING WITHDRAWAL CRITERIA', 'Any other Protocol Deviation where a subject was not withdrawn from the study after meeting requirement of the withdrawal criteria'),
+  ('STUDY PROCEDURE', 'ACTIVITY LEVEL ABOVE PROTOCOL SPECIFICATION', 'A protocol deviation where activity level was above protocol specification'),
+  ('STUDY PROCEDURE', 'BIOLOGICAL SAMPLE SPECIMEN PROCEDURE', 'A protocol deviation in the protocol-specified biological sample specimen procedures.'),
+  ('STUDY PROCEDURE', 'DECLINED PARTICIPATION IN STUDY PROCEDURE', 'A protocol deviation where subject declined participation in study procedure'),
+  ('STUDY PROCEDURE', 'DIARY PROCEDURE', 'A protocol deviation in the protocol-specified diary procedures.'),
+  ('STUDY PROCEDURE', 'DISCONTINUED PARTICIPATION IN STUDY PROCEDURE', 'A protocol deviation where subject discontinued participation in study procedure'),
+  ('STUDY PROCEDURE', 'EQUIPMENT PROCEDURE', 'A protocol deviation in the protocol-specified equipment procedures.'),
+  ('STUDY PROCEDURE', 'NON STUDY TREATMENT SUPPLY PROCEDURE', 'A protocol deviation in the protocol-specified non study treatment supply procedures.'),
+  ('STUDY PROCEDURE', 'NONCOMPLIANCE WITH STUDY PROCEDURE', 'A protocol deviation where subject was not in compliance with at least one study procedure'),
+  ('STUDY PROCEDURE', 'POST STUDY TREATMENT OBSERVATION NOT DONE', 'A protocol deviation where the protocol-specified post study treatment observation was not done.'),
+  ('STUDY PROCEDURE', 'RANDOMIZATION PROCEDURE (E.G. SUBJECT ASSIGNED TO WRONG STRATUM, SUBJECT RANDOMIZED OUT OF ORDER)', 'A protocol deviation in the randomization procedure (e.g., subject assigned to wrong stratum, subject randomized out of order).'),
+  ('STUDY PROCEDURE', 'STUDY BLINDING/UNBLINDING PROCEDURE', 'A protocol deviation in any protocol-specified study blinding or unblinding  procedure.'),
+  ('STUDY PROCEDURE', 'OTHER DEVIATION FROM STUDY PROCEDURE', 'A protocol deviation in any other protocol-specified study procedure.'),
+  ('VISIT COMPLETION', 'MISSED VISIT/PHONE CONTACT', 'Protocol deviation where the subject''s protocol-specified visit or phone contact was missed'),
+  ('VISIT COMPLETION', 'OUT OF WINDOW - VISIT/PHONE CONTACT', 'Protocol deviation where the subject''s protocol-specified visit or phone contact was conducted outside of the time window specified in the protocol'),
+  ('VISIT COMPLETION', 'OTHER VISIT WINDOW DEVIATION', 'Any other protocol deviation associated with a protocol-specified visit window'),
+  ('WRONG STUDY TREATMENT/ADMINISTRATION/DOSE', 'EXPIRED STUDY TREATMENT ADMINISTERED', 'Protocol Deviation where a subject was administered expired study treatment'),
+  ('WRONG STUDY TREATMENT/ADMINISTRATION/DOSE', 'STUDY TREATMENT ADMINISTERED WHILE CONTRAINDICATION', 'Protocol Deviation where a subject was administered study treatment even though there was a contraindication to receiving the study treatment.'),
+  ('WRONG STUDY TREATMENT/ADMINISTRATION/DOSE', 'STUDY TREATMENT NOT ADMINISTERED PER PROTOCOL', 'Protocol Deviation where a subject was not administered study treatment per protocol requirements.'),
+  ('WRONG STUDY TREATMENT/ADMINISTRATION/DOSE', 'STUDY TREATMENT NOT AVAILABLE AT SITE FOR ADMINISTRATION', 'Protocol Deviation where the study treatment was not available on site for administration to the subject.'),
+  ('WRONG STUDY TREATMENT/ADMINISTRATION/DOSE', 'STUDY TREATMENT NOT PREPARED AS PER PROTOCOL (E.G. RECONSTITUTION)', 'Protocol Deviation where the study treatment was not prepared following the directions in the protocol.'),
+  ('WRONG STUDY TREATMENT/ADMINISTRATION/DOSE', 'USE OF STUDY TREATMENT IMPACTED BY TEMPERATURE EXCURSION - NOT REPORTED/APPROVED/DISAPPROVED FOR FURTHER USE', 'Protocol Deviation where study medication was used even though the study treatment was maintained outside the temperature range allowed by the protocol.'),
+  ('WRONG STUDY TREATMENT/ADMINISTRATION/DOSE', 'WRONG STUDY TREATMENT OR ASSIGNMENT ADMINISTERED', 'Protocol Deviation where the wrong study treatment was administered to the subject.'),
+  ('WRONG STUDY TREATMENT/ADMINISTRATION/DOSE', 'OTHER DEVIATION RELATED TO WRONG STUDY TREATMENT/ADMINISTRATION/DOSE', 'Any other Protocol Deviation where a subject was administered the wrong study treatment, wrong study treatment administration and/or wrong dose.'),
+  ('SITE LEVEL ERROR', 'ERRORS IN DELEGATION  LOG COMPLETION', 'Protocol deviation due to error in delegation log completion'),
+  ('SITE LEVEL ERROR', 'ERRORS IN SITE FILE COMPLETION', 'Protocol deviation due to error in site file completion'),
+  ('SITE LEVEL ERROR', 'ERRORS IN DOCUMENTATION FOR TRAINING', 'Protocol deviation due to other errors with documentation for training'),
+  ('SITE LEVEL ERROR', 'OTHER SITE LEVEL DOCUMENTATION ERRORS', 'Protocol deviation due to other site level documentation errors');
--- src/main/java/org/digitalecmt/qualityassurance/App.java
+++ src/main/java/org/digitalecmt/qualityassurance/App.java
@@ -3,6 +3,7 @@ package org.digitalecmt.qualityassurance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
