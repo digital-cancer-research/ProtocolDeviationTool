7d16ef88fe75e3df268824008015d59340ce8b50
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2024-07-23 11:02:33 +0100

    Make bar graph per category at site responsive
    QADP-146: Make visualisation page responsive.

--- src/main/frontend/src/app/category-bar-graph/category-bar-graph.component.ts
+++ src/main/frontend/src/app/category-bar-graph/category-bar-graph.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 import { CategoryBarGraphService } from './category-bar-graph.service';
 import { EntryCountPerCategoryDTO } from './category-bar-graph.model';
 import * as d3 from 'd3';
 import { scaleLinear, scaleBand, max, ValueFn } from 'd3';
 import { ShareSiteDataService } from '../site-select/share-site-data.service';
+import { DimensionService } from '../services/dimension-service.service';
 
 @Component({
   selector: 'app-category-bar-graph',
@@ -11,113 +12,115 @@ import { ShareSiteDataService } from '../site-select/share-site-data.service';
   styleUrls: ['./category-bar-graph.component.css']
 })
 export class CategoryBarGraphComponent implements OnInit {
-  entryCountPerCategory: EntryCountPerCategoryDTO[] = [];
-  selectedSiteId?: string;
+	entryCountPerCategory: EntryCountPerCategoryDTO[] = [];
+	selectedSiteId?: string;
 
-  constructor(private elementRef: ElementRef,
-		  private categoryBarGraphService: CategoryBarGraphService, 
-		  private shareSiteDataService: ShareSiteDataService) {}
+	constructor(private elementRef: ElementRef,
+		private categoryBarGraphService: CategoryBarGraphService, 
+		private shareSiteDataService: ShareSiteDataService) {}
 
-  ngOnInit() {
-	  this.shareSiteDataService.selectedSiteId$.subscribe((siteId: string | undefined) => {
+	ngOnInit() {
+		this.shareSiteDataService.selectedSiteId$.subscribe((siteId: string | undefined) => {
 	  	this.selectedSiteId = siteId;
 	  	this.loadEntryCountPerCategory();
 	});
   }
 
-  loadEntryCountPerCategory() {
-    this.categoryBarGraphService.getEntryCountPerCategory(this.selectedSiteId).subscribe(
-      data => {
-        this.entryCountPerCategory = data;
-        this.createD3BarGraph();
-        
-      },
-      error => {
-        console.error('Error loading data: ', error);
-      }
+  	@HostListener('window:resize', ['$event'])
+  	onResize(event: Event) {
+		  this.createD3BarGraph();
+	}
+	
+	loadEntryCountPerCategory() {
+		this.categoryBarGraphService.getEntryCountPerCategory(this.selectedSiteId).subscribe(
+      	data => {
+        	this.entryCountPerCategory = data;
+			this.createD3BarGraph();
+		},
+		error => {
+			console.error('Error loading data: ', error);
+		}
     );
   }
 
   createD3BarGraph() {
-	  d3.select('#categoryBarGraph').selectAll('*').remove();
-	  const nativeElement = this.elementRef.nativeElement;
-	  const margin = { top: 50, right: 20, bottom: 60, left: 400 };
-	  const width = 1000 - margin.left - margin.right;
-	  const height = 350 - margin.top - margin.bottom;
+	const nativeElement = this.elementRef.nativeElement;
+	const div = d3.select("div.barContent");
+	div.selectChild('svg').remove();
+	DimensionService.getHTMLDimensions(div);
+	const divDimensions = DimensionService.getHTMLDimensions(div);
+	const width = 0.8*divDimensions.width;
+	const height = 0.75*divDimensions.height;
+	const margin = { top: 0.05*height, 
+				   right: -0.35*width,
+			 	  bottom: 0.2*height, 
+				    left: 0.4*width
+				};
 
-	  const svg = d3
-	    .select('#categoryBarGraph')
-	    .append('svg')
-	    .attr('width', width + margin.left + margin.right)
-	    .attr('height', height + margin.top + margin.bottom)
-	    .append('g')
-	    .attr('transform', `translate(${margin.left},${margin.top})`);
+	const svg = div
+	.append('svg')
+	.attr('width', width + margin.left + margin.right)
+	.attr('height', height + margin.top + margin.bottom)
+	.append('g')
+	.attr('transform', `translate(${(margin.left - margin.right)/1.5},${margin.top})`);
+	
+	if (width < 684) {
+		div.
+		select('svg')
+		.attr('viewBox', `-20 0 ${width + margin.left + margin.right} ${height*1.1 + margin.top + margin.bottom}`)
+	}
 
-	  const x = d3
-	    .scaleLinear()
-	    .domain([0, d3.max(this.entryCountPerCategory, (d) => d.entryCount) || 0])
-	    .range([0, width]);
-
-	  const y = d3
-	    .scaleBand<string>()
-	    .range([height, 0])
-	    .domain(this.entryCountPerCategory.map((d) => d.dvcat || ''))
-	    .padding(0.1);
-
-	  const yAttr: ValueFn<SVGRectElement, EntryCountPerCategoryDTO, string | number | boolean | null> =
-	    (d) => (d.dvcat ? y(d.dvcat) || 0 : 0);
-
-	  // Add title
-	  svg.append('text')
-	    .attr('x', width / 2)
-	    .attr('y', -margin.top / 2)
-	    .attr('text-anchor', 'middle')
-	    .style('font-size', '20px')
-	    .text('Total number of PDs per category (DVCAT) at site');
-
-	  // Add X axis label
-	  svg.append('text')
-	    .attr('x', width / 2)
-	    .attr('y', height + margin.bottom / 1.5)
-	    .attr('text-anchor', 'middle')
-	    .style('font-size', '14px')
-	    .text('Total number of PDs per category');
-
-	  // Add Y axis label
-	  svg.append('text')
-	    .attr('transform', 'rotate(-90)')
-	    .attr('x', -height / 2)
-	    .attr('y', -margin.left / 1.1)
-	    .attr('text-anchor', 'middle')
-	    .style('font-size', '14px')
-	    .text('Category for Protocol Deviation');
-
-	 // Calculate the number of ticks for the x-axis
-	    const maxTicks = Math.min(10, this.entryCountPerCategory.length);
-
-	    // Add X axis
-	    svg.append('g')
-	       .attr('transform', `translate(0, ${height})`)
-	       .call(d3.axisBottom(x)
-	       .ticks(maxTicks)
-	       .tickFormat(d3.format('d')));
-	  
-	  // Add Y axis
-	  svg.append('g')
-	    .call(d3.axisLeft(y));
-
-	  
-	  const tooltip = d3.select(nativeElement).append('div')
-			    .attr('class', 'tooltip')
-			    .style('opacity', 0)
-			    .style("position", "absolute")
-			    .style("background-color", "white")
-			    .style("border", "solid")
-			    .style("border-width", "1px")
-			    .style("border-radius", "5px")
-			    .style("padding", "10px")
+	const maxEntry = d3.max(this.entryCountPerCategory, (d) => d.entryCount) || 0;
+  	const x = d3
+	.scaleLinear()
+	.domain([0, maxEntry])
+	.range([0, width/1.5 - margin.left - margin.right/3]);
+	svg.append('g')
+	.attr("transform", "translate(0," + height + ")")
+	.call(d3.axisBottom(x));
+	
+	if (height > 148) {
+		svg.append('text')
+			.attr('x', (width  - margin.left)/3)
+			.attr('y', height + margin.bottom - 5)
+			.attr('text-anchor', 'middle')
+			.style('font-size', '2vh')
+			.text('Total number of PDs per category');
+	}
+	else {
+		svg.append('text')
+			.attr('x', (width  - margin.left)/3)
+			.attr('y', height + margin.bottom + 2.5)
+			.attr('text-anchor', 'middle')
+			.style('font-size', '2vh')
+			.text('Total number of PDs per category');
 		
-			    
+	}
+
+  const y = d3
+	.scaleBand()
+	.range([0, height])
+	.domain(this.entryCountPerCategory.map((d) => d.dvcat || ''))
+	.padding(0.1);
+
+	svg.append('g')
+		.call(d3.axisLeft(y))
+		.selectAll('text')
+		.style('font-size', '0.75vw');
+
+	const yAttr: ValueFn<SVGRectElement, EntryCountPerCategoryDTO, string | number | boolean | null> =
+	(d) => (d.dvcat ? y(d.dvcat) || 0 : 0);
+
+	const tooltip = d3.select(nativeElement).append('div')
+			.attr('class', 'tooltip')
+			.style('opacity', 0)
+			.style("position", "absolute")
+			.style("background-color", "white")
+			.style("border", "solid")
+			.style("border-width", "1px")
+			.style("border-radius", "5px")
+			.style("padding", "10px")
+		
 	const color = d3.scaleOrdinal(d3.schemeCategory10);
 			    
 	 // Add bars
@@ -150,10 +153,10 @@ export class CategoryBarGraphComponent implements OnInit {
 	    	.style('top', (yPos - 20) + 'px');
 	    })
 	    
-	      .on('mouseout', function (d) {
-	        tooltip
-	          .style('opacity', 0);
-	      });
+		.on('mouseout', function (d) {
+		tooltip
+			.style('opacity', 0);
+		});
 	}
 
 
