f9896287f3c16e8db72184d53395348340b13f80
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2024-07-23 11:04:52 +0100

    Make bar graph per category per study at site responsive
    QADP-146: Make visualisation page responsive.

--- src/main/frontend/src/app/category-bar-graph-segmented/category-bar-graph-segmented.component.ts
+++ src/main/frontend/src/app/category-bar-graph-segmented/category-bar-graph-segmented.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 import { CategoryBarGraphSegmentedService } from './category-bar-graph-segmented.service';
 import { EntryCountPerCategoryPerStudyDTO } from './category-bar-graph-segmented.model';
 import * as d3 from 'd3';
 import { scaleLinear, scaleBand, max, ValueFn } from 'd3';
 import { ShareSiteDataService } from '../site-select/share-site-data.service';
+import { DimensionService } from '../services/dimension-service.service';
 
 @Component({
   selector: 'app-category-bar-graph-segmented',
@@ -13,153 +14,155 @@ import { ShareSiteDataService } from '../site-select/share-site-data.service';
 
 export class CategoryBarGraphSegmentedComponent implements OnInit {
 	entryCountPerCategoryPerStudy: EntryCountPerCategoryPerStudyDTO[] = [];
-	  selectedSiteId?: string;
+	selectedSiteId?: string;
 
-	  constructor(private elementRef: ElementRef,
+	constructor(private elementRef: ElementRef,
 	    private categoryBarGraphSegmentedService: CategoryBarGraphSegmentedService,
 	    private shareSiteDataService: ShareSiteDataService) { }
 
-	  ngOnInit() {
+	ngOnInit() {
 	    this.shareSiteDataService.selectedSiteId$.subscribe((siteId: string | undefined) => {
-	      this.selectedSiteId = siteId;
-	      this.loadEntryCountPerCategoryPerStudy();
+			this.selectedSiteId = siteId;
+			this.loadEntryCountPerCategoryPerStudy();
 	    });
-	  }
+	}
 
-	  loadEntryCountPerCategoryPerStudy() {
-	    this.categoryBarGraphSegmentedService.getEntryCountPerCategoryPerStudy(this.selectedSiteId).subscribe(
-	      data => {
-	        this.entryCountPerCategoryPerStudy = data;
-	        console.log(this.entryCountPerCategoryPerStudy);
+	@HostListener('window:resize', ['$event'])
+	onResize(event: Event) {
+		this.createD3BarGraph();
+	}
 
-	        this.createD3BarGraph();
-
-	      },
-	      error => {
-	        console.error('Error loading data: ', error);
-	      }
+	loadEntryCountPerCategoryPerStudy() {
+		this.categoryBarGraphSegmentedService
+		.getEntryCountPerCategoryPerStudy(this.selectedSiteId).subscribe(
+	    	data => {
+	        	this.entryCountPerCategoryPerStudy = data;
+				this.createD3BarGraph();
+	      	},
+	    	error => {
+	        	console.error('Error loading data: ', error);
+	      	}
 	    );
 	  }
 
-	  createD3BarGraph() {
-		  d3.select(this.elementRef.nativeElement).selectAll('*').remove();
-		    const margin = { top: 45, right: 30, bottom: 100, left: 120 };
-		    const width = 850 - margin.left - margin.right;
-		    const height = 400 - margin.top - margin.bottom;
+	createD3BarGraph() {
+		const nativeElement = this.elementRef.nativeElement;
+		const div = d3.select('div.histogramContent');
+		div.selectChild('svg').remove();
+		DimensionService.getHTMLDimensions(div);
+		const divDimensions = DimensionService.getHTMLDimensions(div);
+		const width = 0.6*divDimensions.width;
+		const height = 0.75*divDimensions.height;
+		const margin = { top: 0.05*height, 
+					   right: 0.1*width,
+					  bottom: 0.3*height,
+					    left: 0.3*width};
 
-		    // Append SVG to the component's element
-		    const svg = d3.select(this.elementRef.nativeElement).append('svg')
-		      .attr('width', width + margin.left + margin.right)
-		      .attr('height', height + margin.top + margin.bottom)
-		      .append('g')
-		      .attr('transform', `translate(${margin.left},${margin.top})`);
-		    
-			 // Calculate the total entry count for each study
-		    const totalEntryCounts = this.entryCountPerCategoryPerStudy.map(entry => ({
-		      studyId: entry.studyId,
-		      totalEntryCount: d3.sum(this.entryCountPerCategoryPerStudy.filter(d => d.studyId === entry.studyId), d => d.entryCount)!
-		    }));
-
-		    // Define scales
-		    const xScale = d3.scaleLinear()
-		      .domain([0, d3.max(totalEntryCounts, d => d.totalEntryCount)!])
-		      .range([0, width]);
-
-		    const yScale = d3.scaleBand()
-		      .domain(this.entryCountPerCategoryPerStudy.map(d => d.studyId))
-		      .range([height, 0])
-		      .padding(0.1);
-
-		    // Define color scale
-		    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-
-		    // Create stacked bars
-		    svg.selectAll('.bar')
-		      .data(this.entryCountPerCategoryPerStudy)
-		      .enter().append('rect')
-		      .attr('class', 'bar')
-		      .attr('x', d => {
-		    	    let sum = 0;
-		    	    // Calculate the cumulative entry count up to the current category
-		    	    for (const entry of this.entryCountPerCategoryPerStudy.filter(e => e.studyId === d.studyId)) {
-		    	      if (entry.dvcat === d.dvcat) break;
-		    	      sum += entry.entryCount;
-		    	    }
-		    	    return xScale(sum);
-		    	  })
-		      .attr('y', d => yScale(d.studyId)!)
-		      .attr('width', d => xScale(d.entryCount)!)
-		      .attr('height', yScale.bandwidth())
-		      .style('fill', d => colorScale(d.dvcat))
-		      .on('mouseover', function (event, d) {
-		            const xPos = event.pageX;
-		            const yPos = event.pageY;
-
-		            tooltip
-		                .style('opacity', 1)
-		                .style('left', (xPos + 20) + 'px')
-		                .style('top', (yPos - 20) + 'px')
-		                .html(`<strong>${d.dvcat}</strong><br>Count: ${d.entryCount}`)
-
-		        })
-		        .on('mousemove', function (event, d) {
-		            const xPos = event.pageX;
-		            const yPos = event.pageY;
-
-		            tooltip
-		                .style('left', (xPos + 20) + 'px')
-		                .style('top', (yPos - 20) + 'px');
-		        })
-		        .on('mouseout', function (d) {
-		            tooltip
-		                .style('opacity', 0);
-		        });
-
-		    // Add y-axis
-		    svg.append('g')
-		      .attr('class', 'axis')
-		      .call(d3.axisLeft(yScale));
-
-		    // Add x-axis
-		    svg.append('g')
-		      .attr('class', 'axis')
-		      .attr('transform', `translate(0,${height})`)
-		      .call(d3.axisBottom(xScale).ticks(5));
-
-		    // Add chart title
-		    svg.append('text')
-		      .attr('x', width / 2)
-		      .attr('y', -margin.top / 2)
-		      .style('font-size', '20px')
-		      .attr('text-anchor', 'middle')
-		      .text('Total number of PDs per category (DVCAT) per study at site');
-
-		    // Add Y axis label
-		    svg.append('text')
-		      .attr('transform', 'rotate(-90)')
-		      .attr('x', -height / 2)
-		      .attr('y', -margin.left + 30)
-		      .attr('text-anchor', 'middle')
-		      .text('Study ID');
-		    
-			 // Add X axis label
-		    svg.append('text')
-		        .attr('x', width / 2)
-		        .attr('y', height + margin.bottom - 40)
-		        .attr('text-anchor', 'middle')
-		        .style('font-size', '14px')
-		        .text('Total number of PDs per DVCAT');
-
-
-		    // Define tooltip
-		    const tooltip = d3.select(this.elementRef.nativeElement).append('div')
-		      .attr('class', 'tooltip')
-		      .style('opacity', 0)
-		      .style('position', 'absolute')
-		      .style('background-color', 'white')
-		      .style('border', 'solid')
-		      .style('border-width', '1px')
-		      .style('border-radius', '5px')
-		      .style('padding', '10px');
-		  }
+		const svg = div
+		.append('svg')
+		.attr('width', width + margin.left - margin.right)
+		.attr('height', height + margin.top + margin.bottom)
+		.append('g')
+		.attr('transform', `translate(${(margin.left - margin.right)/1.5},0)`);
+					
+		if (width < 684) {
+			div.
+			select('svg')
+			.attr('viewBox', `-20 0 ${width + margin.left} ${height + margin.top + margin.bottom}`)
 		}
+
+		// Calculate the total entry count for each study
+		const totalEntryCounts = this.entryCountPerCategoryPerStudy.map(entry => ({
+		    studyId: entry.studyId,
+		    totalEntryCount: d3.sum(this.entryCountPerCategoryPerStudy
+				.filter(d => d.studyId === entry.studyId), d => d.entryCount)!
+		}));
+
+		// Define scales
+		const xScale = d3.scaleLinear()
+			.domain([0, d3.max(totalEntryCounts, d => d.totalEntryCount)!])
+			.range([0, width]);
+
+		const yScale = d3.scaleBand()
+			.domain(this.entryCountPerCategoryPerStudy.map(d => d.studyId))
+			.range([0, height])
+			.padding(0.1);
+
+		// Define color scale
+		const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+		// Create stacked bars
+		svg.selectAll('.bar')
+			.data(this.entryCountPerCategoryPerStudy)
+			.enter().append('rect')
+			.attr('class', 'bar')
+			.attr('x', d => {
+
+				let sum = 0;
+				// Calculate the cumulative entry count up to the current category
+				for (const entry of this.entryCountPerCategoryPerStudy
+					.filter(e => e.studyId === d.studyId)) {
+					if (entry.dvcat === d.dvcat) break;
+					sum += entry.entryCount;
+				}
+				return xScale(sum);
+				})
+			.attr('y', d => yScale(d.studyId)!)
+			.attr('width', d => xScale(d.entryCount)!)
+			.attr('height', yScale.bandwidth())
+			.style('fill', d => colorScale(d.dvcat))
+			.on('mouseover', function (event, d) {
+				const xPos = event.pageX;
+				const yPos = event.pageY;
+		        tooltip
+				.style('opacity', 1)
+				.style('left', (xPos + 20) + 'px')
+				.style('top', (yPos - 20) + 'px')
+				.html(`<strong>${d.dvcat}</strong><br>Count: ${d.entryCount}`)
+		    })
+		    .on('mousemove', function (event, d) {
+				const xPos = event.pageX;
+				const yPos = event.pageY;
+				tooltip
+				.style('left', (xPos + 20) + 'px')
+				.style('top', (yPos - 20) + 'px');
+			})
+			.on('mouseout', function (d) {
+				tooltip
+				.style('opacity', 0);
+			});
+
+		// Add y-axis
+
+		const y = d3
+		.scaleBand()
+		.range([0, height])
+		.domain(this.entryCountPerCategoryPerStudy.map((d) => d.studyId || ''))
+		.padding(0.1);
+
+		svg.append('g')
+		.call(d3.axisLeft(y))
+		.selectAll('text')
+		.style('font-size', '0.75vw');
+
+		// Add x-axis
+		const x = d3
+		.scaleLinear()
+		.domain([0, d3.max(totalEntryCounts, d => d.totalEntryCount)!])
+		.range([0, width]);
+		svg.append('g')
+		.attr("transform", "translate(0," + height + ")")
+		.call(d3.axisBottom(x));
+
+		// Define tooltip
+		const tooltip = d3.select(this.elementRef.nativeElement).append('div')
+			.attr('class', 'tooltip')
+			.style('opacity', 0)
+			.style('position', 'absolute')
+			.style('background-color', 'white')
+			.style('border', 'solid')
+			.style('border-width', '1px')
+			.style('border-radius', '5px')
+			.style('padding', '10px');
+	}
+}
