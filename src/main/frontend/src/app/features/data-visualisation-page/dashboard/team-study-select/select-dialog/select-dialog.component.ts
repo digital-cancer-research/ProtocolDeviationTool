8ace20881e9e71e5b591529bc6250102bd5086e2
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2024-10-28 16:28:11 +0000

    Encode study id in url
    Encode study id when selected from dialogue popup.

--- src/main/frontend/src/app/features/data-visualisation-page/dashboard/team-study-select/select-dialog/select-dialog.component.ts
+++ src/main/frontend/src/app/features/data-visualisation-page/dashboard/team-study-select/select-dialog/select-dialog.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DetailedViewComponent } from '../../../detailed-view/detailed-view.component';
+import { DataVisualisationPageModule } from '../../../data-visualisation-page.module';
 
 @Component({
   selector: 'app-select-dialog',
@@ -8,6 +11,21 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class SelectDialogComponent {
   data = inject(MAT_DIALOG_DATA);
-
   selectedStudy: string = "";
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  navigate() {
+    this.router.navigate([`${DataVisualisationPageModule.URL}/${DetailedViewComponent.URL}`],
+      {
+        queryParams: {
+          studyId: this.selectedStudy ? this.selectedStudy : null
+        },
+        queryParamsHandling: 'merge'
+      }
+    )
+  }
 }
--- src/main/frontend/src/app/features/data-visualisation-page/detailed-view/detailed-view.component.ts
+++ src/main/frontend/src/app/features/data-visualisation-page/detailed-view/detailed-view.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './detailed-view.component.css'
 })
 export class DetailedViewComponent {
-
+  public static readonly URL = 'detailed-view';
 }
