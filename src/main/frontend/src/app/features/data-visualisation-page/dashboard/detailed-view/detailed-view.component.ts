c2e0f3bf94ebc8bdb54663fd9950c671340d25f2
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2024-11-04 14:00:24 +0000

    Add options for filters
    Add options for filters and bind them to the selected option.
    QADP-214: Add filter for studies on team visualisation.

--- src/main/frontend/src/app/features/data-visualisation-page/dashboard/detailed-view/detailed-view.component.ts
+++ src/main/frontend/src/app/features/data-visualisation-page/dashboard/detailed-view/detailed-view.component.ts
@@ -3,6 +3,10 @@ import { Tab } from 'src/app/shared/tab/tab';
 import { TotalPdsComponent } from './total-pds/total-pds.component';
 import { TotalPdsOverTimeComponent } from './total-pds-over-time/total-pds-over-time.component';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
+import { Team } from 'src/app/core/models/team.model';
+import { StudyService } from 'src/app/core/services/study.service';
+import { map, Observable, of, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-detailed-view',
@@ -12,6 +16,9 @@ import { ActivatedRoute } from '@angular/router';
 export class DetailedViewComponent {
   private static _URL: string = "detailed-view";
   private static _studyId?: string;
+  protected options$: Observable<string[]> = new Observable();
+  protected selectedOption: string = "";
+
   protected tabs: Tab[] = [
     {
       label: "Total Protocol Deviations",
@@ -23,10 +30,40 @@ export class DetailedViewComponent {
     }
   ]
 
-  constructor(route: ActivatedRoute) {
-    route.queryParams.subscribe(params => {
-      DetailedViewComponent._studyId = params['studyId'];
-    })
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private studyService: StudyService
+  ) {
+    this.populateFilterOptions();
+  }
+
+  populateFilterOptions() {
+    this.options$ = this.userService.currentUserSelectedTeam$.pipe(
+      switchMap(team => {
+        this.parseStudyInUrl(team);
+        if (team) {
+          return this.studyService.getStudiesForTeam(team.teamId).pipe(
+            map(studies => [team.teamName, ...studies])
+          );
+        } else {
+          return of([]);
+        }
+      })
+    );
+  }
+
+  parseStudyInUrl(team: Team | null) {
+    this.route.queryParams.subscribe(params => {
+      const studyId = params['studyId'];
+      DetailedViewComponent._studyId = studyId;
+
+      if (studyId === "" || studyId === null || studyId === undefined) {
+        this.selectedOption = team ? team.teamName : "";
+      } else {
+        this.selectedOption = studyId;
+      }
+    });
   }
 
   public static get URL() {
