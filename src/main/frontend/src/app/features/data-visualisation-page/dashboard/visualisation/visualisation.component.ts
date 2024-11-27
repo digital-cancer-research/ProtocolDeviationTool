c9ad0c640edfad1ba370c15d7c29ff468054a009
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2024-11-27 16:21:40 +0000

    Add team name in visualisation
    QADP-46: As a Data Manager I can see the total number of PDs that have occurred at team across all the studies, so that I can monitor how many PDs are occurring at site.

--- src/main/frontend/src/app/features/data-visualisation-page/dashboard/visualisation/visualisation.component.ts
+++ src/main/frontend/src/app/features/data-visualisation-page/dashboard/visualisation/visualisation.component.ts
@@ -1,44 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { VisualisationService } from './visualisation.service';
+import { UserService } from 'src/app/core/services/user.service';
+import { mergeMap, of } from 'rxjs';
+import { Team } from 'src/app/core/models/team.model';
 
 @Component({
 	selector: 'app-visualisation',
 	templateUrl: './visualisation.component.html',
-	styleUrls: ['./visualisation.component.css']
+	styleUrls: ['./visualisation.component.scss']
 })
 export class VisualisationComponent implements OnInit {
+	private readonly userService = inject(UserService);
+	private apiRequest = this.userService.currentUserSelectedTeam$.pipe(
+		mergeMap(team => {
+			this.team = team;
+			if (team === null) {
+				return of(null);
+			} else {
+				return this.visualisationService.getPDsForTeam(team.teamId);
+			}
+		})
+	);
+	protected team: Team | null = null;
 	teamPDs!: number;
 	isLoading: boolean = true;
 	error: boolean = false;
-	errorMessage: string = "Failed to load data for total count";
+	errorMessage: string = "";
 
 	constructor(
 		private visualisationService: VisualisationService,
 	) { }
 
 	ngOnInit() {
-		this.visualisationService.getPDsForCurrentTeam().subscribe({
+
+		this.apiRequest.subscribe({
 			next: (teamPDs) => {
 				this.isLoading = true;
-				setTimeout(() => {
-						if (teamPDs) {
-							this.teamPDs = teamPDs;
-							this.isLoading = false;
-						}
-						else {
-							this.teamPDs = 0;
-							this.isLoading = false;
-						}
-					}, 1000);
+				this.teamPDs = 0;
+				if (teamPDs) {
+					this.teamPDs = teamPDs;
+				}
+				else if (!this.team) {
+					this.error = true;
+					this.errorMessage = "No team selected";
+				}
 			},
 			error: (error) => {
-				setTimeout(
-					() => {
-						console.error("There was an error fetching the number of PDs");
-						console.error(error);
-						this.error = true;
-						this.isLoading = false;
-					});
+				this.errorMessage = "Failed to load data for total count";
+				console.error(error);
+				this.error = true;
+				this.isLoading = false;
 			}
 		})
 	}
