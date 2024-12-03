d790d277b1c1c1969151c059d9c85c63d2bbdfdb
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2024-12-03 12:54:02 +0000

    Delete redundant files
    QADP-75: As a user when I select the data upload button as site or sponsor I can see a data upload page, so that I can load data.

--- src/main/frontend/src/app/shared/file-list/file-list.model.ts
+++
@@ -1,6 +0,0 @@
-export interface FileDTO {
-  fileId: number;
-  fileName: string;
-  username: string;
-  dateTimeUploaded: string;
-}
--- src/main/frontend/src/app/shared/upload/upload-response.model.ts
+++
@@ -1,9 +0,0 @@
-export class UploadResponse {
-  message: string;
-  missingCells: string[];
-
-  constructor(message: string, missingCells: string[] = []) {
-    this.message = message;
-    this.missingCells = missingCells;
-  }
-}
