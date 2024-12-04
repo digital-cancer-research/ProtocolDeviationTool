5d8ee8588da06537668270041407d5b469d3e0aa
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2024-12-04 16:00:39 +0000

    Make site management button disabled
    QADP-75: Site Management button to be greyed out

--- src/main/frontend/src/app/shared/tab/tab.ts
+++ src/main/frontend/src/app/shared/tab/tab.ts
@@ -1,20 +1,26 @@
 /**
- * Represents a tab with a label and a link.
+ * Represents a tab with a label, link, disabled state and tooltip.
  *
  * @class Tab
  */
 export class Tab {
-    label: string = "";
-    link: string = '';
+    label: string
+    link: string
+    disabled: boolean
+    tooltip: string
 
-     /**
+    /**
      * Creates an instance of a Tab.
      * 
-     * @param {string} label - The display label for the tab.
-     * @param {string} link - The route or URL that the tab links to.
+     * @param {string} label - The display label for the tab. Defaults to empty string.
+     * @param {string} link - The route or URL that the tab links to. Defaults to empty string.
+     * @param {string} disabled - The disabled state of the button. Defaults to false.
+     * @param {string} tooltip - The tooltip for the button. Defaults to empty string and no tooltip.
      */
-    constructor(label: string = "", link: string = "") {
+    constructor(label: string = "", link: string = "", disabled: boolean = false, tooltip: string = "") {
         this.label = label;
         this.link = link;
+        this.disabled = disabled;
+        this.tooltip = tooltip;
     }
 }
