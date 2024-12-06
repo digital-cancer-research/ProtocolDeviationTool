280406000e820c6bfb222c81bc4f0fc1a3054c04
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2024-12-06 16:03:19 +0000

    Use guards for router protection
    Use disabled page and deactivated user guards to protect pages.
    QADP-254: Footer is outside border when a team is selected.

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -2,14 +2,39 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SiteSponsorPageComponent } from './site-sponsor-page/site-sponsor-page.component';
 import { DataVisualisationPageModule } from './features/data-visualisation-page/data-visualisation-page.module';
+import { deactivatedUserGuard } from './core/guards/deactivated-user.guard';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./features/home-page/home-page.module').then(m => m.HomePageModule) },
-  { path: 'site-sponsor-page', component: SiteSponsorPageComponent },
-  { path: 'site', loadChildren: () => import('./features/site-page/site-page.module').then(m => m.SitePageModule) },
-  { path: DataVisualisationPageModule.URL, loadChildren: () => import('./features/data-visualisation-page/data-visualisation-page.module').then(m => m.DataVisualisationPageModule) },
-  { path: 'data-upload', loadChildren: () => import('./features/data-upload/data-upload-page.module').then(m => m.DataUploadModule) },
-  { path: 'administration-page', loadChildren: () => import('./features/administration-page/administration-page.module').then(m => m.AdministrationPageModule) },
+  {
+    path: '',
+    loadChildren: () => import('./features/home-page/home-page.module').then(m => m.HomePageModule),
+    canActivate: [deactivatedUserGuard]
+  },
+  {
+    path: 'site-sponsor-page',
+    component: SiteSponsorPageComponent,
+    canActivate: [deactivatedUserGuard]
+  },
+  {
+    path: 'site',
+    loadChildren: () => import('./features/site-page/site-page.module').then(m => m.SitePageModule),
+    canActivate: [deactivatedUserGuard]
+  },
+  {
+    path: DataVisualisationPageModule.URL, loadChildren: () => import('./features/data-visualisation-page/data-visualisation-page.module').then(m => m.DataVisualisationPageModule),
+
+    canActivate: [deactivatedUserGuard]
+  },
+  {
+    path: 'data-upload',
+    loadChildren: () => import('./features/data-upload/data-upload-page.module').then(m => m.DataUploadModule),
+    canActivate: [deactivatedUserGuard]
+  },
+  {
+    path: 'administration-page',
+    loadChildren: () => import('./features/administration-page/administration-page.module').then(m => m.AdministrationPageModule),
+    canActivate: [deactivatedUserGuard]
+  },
 ];
 
 @NgModule({
--- src/main/frontend/src/app/app.component.ts
+++ src/main/frontend/src/app/app.component.ts
@@ -1,12 +1,4 @@
-import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs/operators';
-import { combineLatest } from 'rxjs';
-import { UserService } from './core/services/user.service';
-import { SitePageComponent } from './features/site-page/site-page.component';
-import { AdministrationPageComponent } from './features/administration-page/administration-page.component';
-import { AdministrationPageModule } from './features/administration-page/administration-page.module';
-import { SiteManagementComponent } from './features/administration-page/site-management/site-management.component';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -16,66 +8,6 @@ import { SiteManagementComponent } from './features/administration-page/site-man
 export class AppComponent {
   private title = 'frontend';
 
-  /** 
-   * Indicates whether the footer is visible.
-   * @type {boolean}
-   */
   protected isFooterVisible: boolean = false;
 
-  /**
-   * Creates an instance of AppComponent.
-   * 
-   * Listens for changes in the url and the user to 
-   * manage access to the site and update footer visibility.
-   * 
-   * @param {Router} router - The Angular router instance for navigating between routes.
-   * @param {UserService} userService - The service to manage user-related data and state.
-   */
-  constructor(
-    private router: Router,
-    private userService: UserService
-  ) {
-    const navigationEnd$ = this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    );
-    combineLatest([navigationEnd$, userService.currentUser$])
-      .subscribe(([event, currentUser]) => {
-        event = event as NavigationEnd;
-        this.isFooterVisible = this.updateFooterVisibility(event.url);
-        const lastNavigationUrl = this.router.lastSuccessfulNavigation
-          ?.previousNavigation
-          ?.finalUrl
-          ?.toString() || "";
-
-        if (event.url !== '' && event.url !== `/${SitePageComponent.URL}` && currentUser?.roleId === 3) {
-          this.router.navigateByUrl(lastNavigationUrl);
-        }
-        if (event.url === `/${AdministrationPageModule.URL}/${SiteManagementComponent.URL}`) {
-          this.router.navigateByUrl(lastNavigationUrl);
-        }
-      })
-  }
-
-  /**
-   * Determines whether the footer should be visible based on the current URL.
-   *
-   * @param {string} url - The current URL of the application.
-   * @returns {boolean} True if the footer should be visible, false otherwise.
-   *
-   * @description
-   * This function extracts the root path from the URL and checks if it matches
-   * specific cases where the footer should be visible. It returns true for
-   * the root path ('') and the 'site' path, and false for all other paths.
-   */
-  updateFooterVisibility(url: string): boolean {
-    const urlRoot = url.split('/')[1]
-      .split('?').reverse().pop();
-    switch (urlRoot) {
-      case '':
-      case 'site':
-        return true;
-      default:
-        return false;
-    }
-  }
 }
