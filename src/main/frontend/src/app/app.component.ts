9017ba33993f51a900cbe67fb317e9601d783ca5
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2024-12-06 16:25:55 +0000

    Display footer on appropriate pages
    QADP-254: Footer is outside border when a team is selected.

--- src/main/frontend/src/app/app.component.ts
+++ src/main/frontend/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component, inject } from '@angular/core';
+import { UrlService } from './core/services/url.service';
+import { NavigationEnd } from '@angular/router';
+import { HomePageComponent } from './features/home-page/home-page.component';
+import { SitePageComponent } from './features/site-page/site-page.component';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +12,19 @@ import { Component, inject } from '@angular/core';
 export class AppComponent {
   private title = 'frontend';
 
+  private readonly urlService = inject(UrlService);
+
   protected isFooterVisible: boolean = false;
 
+  private readonly PAGES_WITH_FOOTER = [
+    `/${HomePageComponent.URL}`,
+    `/${SitePageComponent.URL}`
+  ]
+
+  constructor() {
+    this.urlService.newNav$().subscribe(nav => {
+      const url = (nav as unknown as NavigationEnd).url;
+      this.isFooterVisible = this.PAGES_WITH_FOOTER.some(pageUrl => pageUrl.includes(url));
+    })
+  }
 }
