792f78d45d8303094642ff93da5778a5a99b4a52
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2025-01-07 15:20:14 +0000

    Create file table in sqlite

---
+++ db/sqlite/schema/009_create_files.sql
@@ -0,0 +1,6 @@
+CREATE TABLE file (
+    "file_id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "file_name" TEXT NOT NULL,
+    "uploaded_by" INTEGER REFERENCES "user"(user_id) ON DELETE SET NULL ON UPDATE CASCADE,
+    "date_uploaded" TEXT NOT NULL
+);
