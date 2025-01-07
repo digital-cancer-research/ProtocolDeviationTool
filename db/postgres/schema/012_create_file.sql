da712ee1b80ba889abfbdaaf62330b7455a8b64c
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2025-01-07 14:25:03 +0000

    Create file table in postgres

---
+++ db/postgres/schema/012_create_file.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "file" (
+    "file_id" SERIAL PRIMARY KEY,
+    "file_name" VARCHAR NOT NULL,
+    "uploaded_by" INT REFERENCES "user"("user_id") ON DELETE SET NULL ON UPDATE CASCADE,
+    "date_uploaded" VARCHAR NOT NULL, 
+);
