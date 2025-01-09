23fc001217a2bf66e2e09fee71c650073be09e75
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2025-01-09 12:33:16 +0000

    Fix syntax issues with postgres scripts

--- db/postgres/schema/008_create_dvdecod.sql
+++ db/postgres/schema/008_create_dvdecod.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "dvdecod" (
     "dvdecod_id" SERIAL PRIMARY KEY,
-    "dvcat_id" SERIAL PRIMARY KEY REFERENCES "dvcat"("dvcat_id") ON DELETE CASCADE ON UPDATE CASCADE,
+    "dvcat_id" INT REFERENCES "dvcat"("dvcat_id") ON DELETE CASCADE ON UPDATE CASCADE,
     "description" VARCHAR NOT NULL,
     "dvterm" VARCHAR NOT NULL,
     "colour" VARCHAR NOT NULL
--- db/postgres/schema/010_create_file.sql
+++ db/postgres/schema/010_create_file.sql
@@ -2,5 +2,5 @@ CREATE TABLE "file" (
     "file_id" SERIAL PRIMARY KEY,
     "file_name" VARCHAR NOT NULL,
     "uploaded_by" INT REFERENCES "user"("user_id") ON DELETE SET NULL ON UPDATE CASCADE,
-    "date_uploaded" VARCHAR NOT NULL, 
+    "date_uploaded" VARCHAR NOT NULL
 );
