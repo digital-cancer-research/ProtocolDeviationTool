e97673f98ef9342230037d540c5619cb62785e6f
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2025-01-13 11:09:50 +0000

    Remove redundant files

--- src/main/java/org/digitalecmt/qualityassurance/repository/UserRepository.java
+++ src/main/java/org/digitalecmt/qualityassurance/repository/UserRepository.java
@@ -3,7 +3,6 @@ package org.digitalecmt.qualityassurance.repository;
 import java.util.Optional;
 
 import org.digitalecmt.qualityassurance.dto.UserWithRoleDTO;
-import org.digitalecmt.qualityassurance.model.persistence.UserAccount;
 import org.digitalecmt.qualityassurance.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -15,7 +14,7 @@ import org.springframework.data.repository.query.Param;
 @Repository
 public interface UserRepository
         extends JpaRepository<User, Long> {
-	Optional<UserAccount> findByUsername(String username);
+	Optional<User> findByUsername(String username);
 	
 	@Query("SELECT new org.digitalecmt.qualityassurance.dto.UserWithRoleDTO(u.userId, u.username, u.dateCreated, u.roleId, r.roleName) " + 
 		       "FROM UserAccount u " + 	
