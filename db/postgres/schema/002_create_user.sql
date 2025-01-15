d081e797897b8cf1455679d4a93803d352f13bb2
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2025-01-15 14:58:51 +0000

    Fix issue with db

--- db/postgres/schema/002_create_user.sql
+++ db/postgres/schema/002_create_user.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "user" (
     "user_id" SERIAL PRIMARY KEY,
     "username" VARCHAR(255) UNIQUE NOT NULL,
-    "role" role NOT NULL,
+    "role" VARCHAR(11) NOT NULL,
     "is_site" BOOL NOT NULL,
     "is_sponsor" BOOL NOT NULL,
     "date_created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
