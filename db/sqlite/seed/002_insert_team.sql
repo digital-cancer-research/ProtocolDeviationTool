d9e4451855f8fc541eaae46c4ca3a689aad4741a
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2025-03-17 15:07:32 +0000

    Remove seed data for release

--- db/sqlite/seed/002_insert_team.sql
+++ db/sqlite/seed/002_insert_team.sql
@@ -2,26 +2,6 @@ INSERT INTO
     "team" ("team_name", "created_by")
 VALUES
     (
-        'Experimental Cancer Medicine Team',
-        1
-    ),
-    (
-        'Observation & Supportive Research Team',
-        1
-    ),
-    (
-        'Lung Cancer Research Team',
-        1
-    ),
-    (
-        'Breast Cancer Research Team',
-        1
-    ),
-    (
-        'GI Research Team',
-        1
-    ),
-    (
-        'Rare Tumour Research Team',
+        'All Data',
         1
     );
