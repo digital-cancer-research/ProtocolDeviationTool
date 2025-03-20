d498e7f4eb650928ab9d5c382e8f473abb3e1918
Muhammad Shabbir <muhammad.shabbir@manchester.ac.uk>
2025-03-20 14:59:04 +0000

    Disable navigation buttons for a deactivated user
    QADP-233: Add a message when the user is inactive.

--- src/main/frontend/src/app/features/site-page/site-page-navigation-buttons/site-page-navigation-buttons.component.ts
+++ src/main/frontend/src/app/features/site-page/site-page-navigation-buttons/site-page-navigation-buttons.component.ts
@@ -3,14 +3,12 @@ import { DataUploadModule } from '../../data-upload/data-upload-page.module';
 import { DataVisualisationPageModule } from '../../data-visualisation-page/data-visualisation-page.module';
 import { UserService } from 'src/app/core/new/services/user.service';
 import { TeamService } from 'src/app/core/new/services/team.service';
-import { map } from 'rxjs';
-import { Role } from 'src/app/core/new/services/models/user/role.enum';
 
 /**
  * Component for displaying navigation buttons on the site page.
  * 
  * The `SitePageNavigationButtonsComponent` provides a UI for navigating between
- * different sections or functionalities related to the site. It utilizes the `UserService`
+ * different sections or functionalities related to the site. It utilises the `UserService`
  * to access the currently selected team.
  * 
  * @export
@@ -23,25 +21,10 @@ import { Role } from 'src/app/core/new/services/models/user/role.enum';
 })
 export class SitePageNavigationButtonsComponent {
   private readonly teamService = inject(TeamService);
-  private readonly userService = inject(UserService);
   links: string[] = [
     `/${DataUploadModule.URL}`,
     `/${DataVisualisationPageModule.URL}`
   ]
-
-  /**
-   * Boolean flag indicating whether the user is deactivated.
-   * @type {boolean}
-   */
-  private isUserDeactivated$ = this.userService.currentUser$.pipe(
-    map(user => user ? user.role === Role.DEACTIVATED : false)
-  );
-
-  isUserDeactivated: boolean = true;
-
-  constructor() {
-    this.isUserDeactivated$.subscribe(isUserDeactivated => this.isUserDeactivated = isUserDeactivated);
-  }
 
   /**
    * Observable of the currently selected team from the `UserService`.
--- src/main/frontend/src/app/shared/page-button/page-button.component.ts
+++ src/main/frontend/src/app/shared/page-button/page-button.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, inject, Input } from '@angular/core';
 import { QueryParamsHandling } from '@angular/router';
+import { Role } from 'src/app/core/new/services/models/user/role.enum';
+import { UserService } from 'src/app/core/new/services/user.service';
 
 /**
  * @class PageButtonComponent
@@ -22,7 +24,45 @@ export class PageButtonComponent {
   @Input() label: string = "";
   @Input() alt: string = "";
   @Input() link: string = "";
-  @Input() disabled: boolean = false;
+  @Input() disabled?: boolean;
+  @Input() disabledByDeactivatedUser?: boolean;
   @Input() MatIcon: string = "";
   @Input() queryParamsHandling: QueryParamsHandling = 'merge';
+
+  private readonly userService = inject(UserService);
+
+  isCurrentUserDeactivated: boolean = true;
+
+  constructor() {
+    this.checkIfUserIsDeactivated();
+  }
+
+  @HostBinding('class.disabled') get isDisabled() {
+    return this.resolveDisabled();
+  }
+
+  checkIfUserIsDeactivated(): void {
+    this.userService.currentUser$.subscribe(user => {
+      if (user === null) {
+        this.isCurrentUserDeactivated = false;
+      } else {
+        this.isCurrentUserDeactivated = (user.role === Role.DEACTIVATED);
+      }
+    });
+  }
+
+  resolveDisabled(): boolean {
+    if (this.disabled) {
+      if (this.disabledByDeactivatedUser) {
+        return this.disabled || (this.disabled && this.isCurrentUserDeactivated)
+      } else {
+        return this.disabled;
+      }
+    } else {
+      if (this.disabledByDeactivatedUser) {
+        return this.isCurrentUserDeactivated;
+      }
+    }
+    return false;
+  }
 }
